6dba86d2dc2a757c89560a4c05972f6777acf196
jjosebastos <josebezerrabastos@gmail.com>
2025-09-19 19:54:09 -0300

    updated layout and added i18n fields.

--- src/main/java/com/br/code_crafters/config/I18nConfiguration.java
+++ src/main/java/com/br/code_crafters/config/I18nConfiguration.java
@@ -2,6 +2,7 @@ package com.br.code_crafters.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@Configuration
 public class I18nConfiguration implements WebMvcConfigurer {
     @Bean
     MessageSource messageSource(){
